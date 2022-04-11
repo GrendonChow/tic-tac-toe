5ad67d003083f1c63dcce2598905a886c5e15cc0
G Chow <grendon.chow@gmail.com>
2022-04-11 22:53:03 +0800

    Added Player factory

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const gameBoard = (() => {
     let board = [];
 })();
 
-const Player = (name) => {
-
+//Factory for creatign Playyer objects
+const Player = (sign) => { 
+    let name = sign;
+    const getSign = () => sign;
+    const getName = () => name;
+    const setName = inName => {
+        if(inName != "")
+        {
+           name = inName
+        }
+    };
+    return { getSign, getName, setName}
 };
 
 const displayController = (() => {
-
+    //do stuff
 })();
 
-const playerX = Player();
+const gameController = (() =>{
+    const startBtn = document.getElementById("start-button");
+    const playerOne = Player('x');
+    const playerTwo = Player('o');
+
+    startBtn.onclick = () => {
+        const oneInput = document.getElementById("player-one-name");
+        const twoInput = document.getElementById("two");
+        playerOne.setName(oneInput.value);
+        playerTwo.setName(twoInput.value);
+    }
+})();
