8fefff96c0f74866ce22ba6865bf1bf7f86ebd08
G Chow <grendon.chow@gmail.com>
2022-04-11 23:29:04 +0800

    changed style of name inputs

--- script.js
+++ script.js
@@ -20,28 +20,48 @@ const displayController = (() => {
     //do stuff
     const infoDisplay = document.getElementById("info-display");
     const nextTurn = () => infoDisplay.textContent = "Somones turn";
+    const clearBoard = () => console.log("CLEAR BOARD");
+    return{
+        nextTurn,
+        clearBoard,
+    };
 })();
 
 const gameController = (() =>{
     const mainBtn = document.getElementById("start-button");
-
     const playerOne = Player('x');
     const playerTwo = Player('o');
 
     mainBtn.onclick = () => {
+        const oneInput = document.getElementById("player-one-name");
+        const twoInput = document.getElementById("player-two-name");
         if(mainBtn.textContent == "Start")
         {
             mainBtn.setAttribute("id","restart-button");
             mainBtn.textContent = "Restart";
-            console.log("test");
-            const oneInput = document.getElementById("player-one-name");
-            const twoInput = document.getElementById("two");
+
+
+            if(oneInput.value == "")
+            {
+                console.log("ADSASD")
+                oneInput.value = "×";
+            }
+            if(twoInput.value == ""){
+                twoInput.value = "○";
+            }
+
             playerOne.setName(oneInput.value);
             playerTwo.setName(twoInput.value);
+            displayController.nextTurn();
+            oneInput.disabled = true;
+            twoInput.disabled = true;
         }
         else{
             mainBtn.setAttribute("id","start-button");
             mainBtn.textContent = "Start";
+            displayController.clearBoard();
+            oneInput.disabled = false;
+            twoInput.disabled = false;
         }
 
     }
