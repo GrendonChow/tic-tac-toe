cc7a58b6b1df677f14a7cb9b0c330be2f9f36e95
G Chow <grendon.chow@gmail.com>
2022-04-11 23:08:31 +0800

    Added Start and restart button toggle

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gameBoard = (() => {
     let board = [];
 })();
 
-//Factory for creatign Playyer objects
+//Factory for creating Playyer objects
 const Player = (sign) => { 
     let name = sign;
     const getSign = () => sign;
@@ -18,17 +18,31 @@ const Player = (sign) => {
 
 const displayController = (() => {
     //do stuff
+    const infoDisplay = document.getElementById("info-display");
+    const nextTurn = () => infoDisplay.textContent = "Somones turn";
 })();
 
 const gameController = (() =>{
-    const startBtn = document.getElementById("start-button");
+    const mainBtn = document.getElementById("start-button");
+
     const playerOne = Player('x');
     const playerTwo = Player('o');
 
-    startBtn.onclick = () => {
-        const oneInput = document.getElementById("player-one-name");
-        const twoInput = document.getElementById("two");
-        playerOne.setName(oneInput.value);
-        playerTwo.setName(twoInput.value);
+    mainBtn.onclick = () => {
+        if(mainBtn.textContent == "Start")
+        {
+            mainBtn.setAttribute("id","restart-button");
+            mainBtn.textContent = "Restart";
+            console.log("test");
+            const oneInput = document.getElementById("player-one-name");
+            const twoInput = document.getElementById("two");
+            playerOne.setName(oneInput.value);
+            playerTwo.setName(twoInput.value);
+        }
+        else{
+            mainBtn.setAttribute("id","start-button");
+            mainBtn.textContent = "Start";
+        }
+
     }
 })();
